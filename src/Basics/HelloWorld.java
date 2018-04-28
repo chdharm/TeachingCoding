2f6899d508170e880cd33bd6773a884d2e507a47
chdharmtravelyaari <dharmpal.chaudhary@travelyaari.com>
2018-04-28 18:21:13 +0530

    Second commit

---
+++ src/Basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package Basics;
+import java.util.*;
+public class HelloWorld {
+	
+	public static void main(String [] args){
+		Scanner scan=new Scanner(System.in);
+		String name=scan.nextLine();
+		System.out.println("Hello World, Welcome"+name);
+	}
+}
