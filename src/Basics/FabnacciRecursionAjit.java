5a0d5f94cde0ac5033bc546d2dda92782c754b53
Ajitkumar1995 <kajit4416@gmail.com>
2018-04-28 23:38:11 +0530

    i am ajit

--- src/Basics/FabnacciRecursionAjit.java
+++ src/Basics/FabnacciRecursionAjit.java
@@ -1,15 +1,17 @@
 package Basics;
-import java.util.Scanner;
+//import java.util.Scanner;
 public class FabnacciRecursionAjit {
+	
 	public static int  m1(int n)
 	{
 		if(n==0||n==1)
-			return 1;
-	return	(n-1)+(n-2);
+			return n;
+		return	m1(n-1)+m1(n-2);
 	}
+	
 	public static void main(String[] args) {
-   int p=m1(5);
-   System.out.println(p);
+		int p=m1(6);
+		System.out.println(p);
 	}
-
+	
 }
